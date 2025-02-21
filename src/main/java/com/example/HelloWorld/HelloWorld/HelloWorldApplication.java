305b1e2b098ea217f458c8aecdb04192359c1354
rajesha5191 <rajesha5191@gmail.com>
2025-02-21 11:16:55 +0530

    second commit

--- src/main/java/com/example/HelloWorld/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorld/HelloWorld/HelloWorldApplication.java
@@ -1,22 +1,24 @@
-package com.example.HelloWorld.HelloWorld;
+	package com.example.HelloWorld.HelloWorld;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+	import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
-public class HelloWorldApplication {
+	@SpringBootApplication
+	@RestController
+	@RequestMapping("/HelloWorld")
+	public class HelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+		public static void main(String[] args) {
+			SpringApplication.run(HelloWorldApplication.class, args);
+		}
+
+		@GetMapping("/hello")
+		public String helloWorld(){
+
+			return "Say Hello to world...";
+		}
 	}
-
-	@RequestMapping(value = "/hello")
-	public String helloWorld(){
-
-		return "Say Hello to world...";
-	}
-}
 
